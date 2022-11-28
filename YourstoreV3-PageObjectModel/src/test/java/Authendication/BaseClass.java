9e6c70b43e3e69372204b6b2a6b1711a254f3866
white <white@DESKTOP-8O9USS3>
2022-11-28 17:37:09 +0530

    Initial commit

---
+++ YourstoreV3-PageObjectModel/src/test/java/Authendication/BaseClass.java
@@ -0,0 +1,66 @@
+package Authendication;
+
+import java.util.HashMap;  
+import java.util.Map;
+import java.util.Random;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class BaseClass {
+	
+	public final static String driver_Path= "C:\\Users\\white\\OneDrive\\Desktop\\Driver Path\\chromedriver.exe";
+	public static WebDriver driver;	
+	public final static String url = "https://yourstore.io";
+	public final static String name = "Storehook";
+	public final static String mobile_num ="1234567890";
+//	public final static String email = "banuyourstore@gmail.com";
+    public final static String password ="Banu@Yourstore.io";
+    public final static String store_name ="StoreHook";
+	public final static String address = "14, Gulmohar Avenue, Velachery Rd, Guindy, Chennai, Tamil Nadu 600032";
+	public final static String city = "Chennai";
+	public final static String pincode = "600032";
+	public final static String GST = "6358734987664364";
+	public final static String description ="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s";
+	
+	
+	@BeforeSuite
+	public void openBrowser()
+	{
+		System.setProperty("webdriver.chrome.driver", driver_Path);
+		//Create a map to store  preferences 
+				Map<String, Object> prefs = new HashMap<String, Object>();
+				    
+				//add key and value to map as follow to switch off browser notification
+				//Pass the argument 1 to allow and 2 to block
+				prefs.put("profile.default_content_setting_values.notifications", 2);
+				    
+				//Create an instance of ChromeOptions 
+				ChromeOptions options = new ChromeOptions();
+				    
+				// set ExperimentalOption - prefs 
+				options.setExperimentalOption("prefs", prefs);
+				
+				driver = (WebDriver) new ChromeDriver(options);
+				driver.manage().window().maximize();
+	}
+	
+	@BeforeTest
+	public void openUrl()
+	{
+		driver.get(url);
+	}
+
+	
+	
+	
+	
+
+}
---
+++ YourstoreV3-PageObjectModel/src/test/java/Authendication/ProSignup.java
@@ -0,0 +1,119 @@
+package Authendication;
+
+import java.util.Random;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+public class ProSignup extends BaseClass{
+	
+	@Test(description =" signp on pro account", groups="pro")
+	public void proSignupPage() throws InterruptedException
+	{
+		WebDriverWait wait = new WebDriverWait(driver, 20);		
+		
+			
+		System.out.println("Current Url:" + driver.getTitle());
+		
+//		String parent=driver.getWindowHandle();
+//
+//		Set<String>s=driver.getWindowHandles();
+//		
+//		Iterator<String> I1= s.iterator();
+//
+//		while(I1.hasNext())
+//		{
+//
+//		String child_window=I1.next();
+//
+//
+//		if(!parent.equals(child_window))
+//		{
+//		driver.switchTo().window(child_window);
+//
+//		System.out.println(driver.switchTo().window(child_window).getTitle());
+//		public void email()
+		
+			Random rand = new Random();
+			int random = rand.nextInt(100);
+			String email = "banuyourstore@"+ random + "gmail.com";
+		
+		
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@placeholder='Your full name']")));
+		driver.findElement(By.xpath("//input[@placeholder='Your full name']")).sendKeys(name);
+		driver.findElement(By.name("mobile")).sendKeys(mobile_num);
+		driver.findElement(By.name("email")).sendKeys(email);
+		driver.findElement(By.name("password")).sendKeys(password);
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		Thread.sleep(5000);
+	
+		
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='signup-card']/child::app-signup/descendant::form/descendant::input[@name='store_name']")));
+    	driver.findElement(By.xpath("//div[@class='signup-card']/child::app-signup/descendant::form/descendant::input[@name='store_name']")).sendKeys(store_name);
+    	Select s1 = new Select(driver.findElement(By.name("country")));
+    	s1.selectByVisibleText("India");
+    	
+    	Select s2 = new Select(driver.findElement(By.name("currency_code")));
+    	s2.selectByVisibleText("INR");
+    	Thread.sleep(2000);
+    	
+    	driver.findElement(By.xpath("//button[@type='submit']")).click();
+    	Thread.sleep(5000);
+    	
+    	wait.until(ExpectedConditions.elementToBeClickable(By.name("address")));
+    	driver.findElement(By.name("address")).sendKeys(address);
+    	
+    	driver.findElement(By.name("city")).sendKeys(city);
+    	Select s4 = new Select(driver.findElement(By.name("state")));
+    	s4.selectByVisibleText("Tamil Nadu");
+    	
+    	driver.findElement(By.name("pincode")).sendKeys(pincode);
+    	
+    	driver.findElement(By.name("gst_no")).sendKeys(GST);
+    	driver.findElement(By.xpath("//button[@type='submit']")).click();
+    	Thread.sleep(5000);
+    	
+    	Select s5 = new Select(driver.findElement(By.name("service_type")));
+    	s5.selectByIndex(1);
+    	
+    	Select s6 = new Select(driver.findElement(By.name("category")));
+    	s6.selectByVisibleText("Clothing");
+    	
+//    	driver.findElement(By.xpath("//button[@type='submit']")).click();
+//    	Thread.sleep(3000);
+    	
+//    	Select s6 = new Select(driver.findElement(By.id("project-category")));
+//    	s6.selectByIndex(2);
+
+    	wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='signup-card']/child::app-signup/descendant::form/child::div/child::div[2]/descendant::textarea")));
+    	
+    	driver.findElement(By.xpath("//div[@class='signup-card']/child::app-signup/descendant::form/child::div/child::div[2]/descendant::textarea")).sendKeys(description);
+    	
+    	driver.findElement(By.xpath("//button[@type='submit']")).click();
+    	
+    	Thread.sleep(3000);
+    	
+//    	driver.findElement(By.xpath("//button[contains(text(),'Login')]")).click();
+//    	
+//    	driver.findElement(By.name("email")).sendKeys(email);
+//		driver.findElement(By.name("password")).sendKeys(password);
+//		driver.findElement(By.xpath("/html/body/app-root/app-auth-layout/div/div/app-signin/div/div/div/div/form/div[3]/div[3]/btn-loading/button")).click();
+//		
+//		 WebElement error = driver.findElement(By.xpath("//div[@class='auth-content']/descendant::form/child::div/following-sibling::div[2]/child::p"));
+//		 if(error.isEnabled()) {
+//			 System.out.println("error should come if store is not activated" + error.getText());
+//		 }else {
+//			 System.out.println("No error found");
+//		 }
+    	
+    	   	
+    	}
+	//}
+//}		
+
+
+}
