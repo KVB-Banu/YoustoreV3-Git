077806251977e4e9911a948773f0ece55b6e5e0f
white <white@DESKTOP-8O9USS3>
2022-11-30 14:40:34 +0530

    hdsf fkdhd

--- YourstoreV3-PageObjectModel/src/test/java/Authentication/Prosignupbutton.java
+++ YourstoreV3-PageObjectModel/src/test/java/Authentication/Prosignupbutton.java
@@ -2,6 +2,8 @@ package Authentication;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 public class Prosignupbutton extends BaseClass{
@@ -9,10 +11,11 @@ public class Prosignupbutton extends BaseClass{
 	@Test(groups="pro")
 	public void proSignup()
 	{
+		WebDriverWait wait = new WebDriverWait(driver, 50);
 		driver.findElement(By.xpath("/html/body/section[1]/div/div/div[1]/div/div[2]/a/button")).click();
 		JavascriptExecutor obj = (JavascriptExecutor) driver;
 		obj.executeScript("window.scrollBy(0,500)");
-		
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[contains(text(),'Sign up')]")));
 		driver.findElements(By.xpath("//button[contains(text(),'Sign up')]")).get(1).click(); // start button=> pro sign up
 	}
 
